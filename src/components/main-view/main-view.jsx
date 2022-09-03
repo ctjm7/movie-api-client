209a2c9557da0e9c5aed332e3b57fb016504f640
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-09-03 16:53:33 -0700

    added database for movies and created login and registration for users

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,33 +10,62 @@ export class MainView extends React.Component {
 
   constructor(){
     super();
-    this.state = {
-      movies: [
-        { _id: 1, Title: 'Saving Private Ryan', Description: 'Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.', ImagePath: 'https://www.imdb.com/title/tt0120815/mediaviewer/rm1924732160/?ref_=tt_ov_i'},
-        { _id: 2, Title: 'This is Spinal Tap', Description: "Spinal Tap, one of England's loudest bands, is chronicled by film director Marty DiBergi on what proves to be a fateful tour.", ImagePath: 'https://www.imdb.com/title/tt0088258/mediaviewer/rm4265401600/?ref_=tt_ov_i'},
-        { _id: 3, Title: 'Stand by Me', Description: 'After the death of one of his friends, a writer recounts a childhood journey with his friends to find the body of a missing boy.', ImagePath: 'https://www.imdb.com/title/tt0092005/mediaviewer/rm3808838912/?ref_=tt_ov_i'}
-      ],
-      selectedMovie: null
-    }
+      this.state = {
+        movies: [],
+        selectedMovie: null,
+        user: null,
+        register: null
+      };
   }
 
+  componentDidMount(){
+    axios.get('https://seeyouatmovies.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie */
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
-  render() {
-    const {movies, selectedMovie} = this.state;
+  /* When a user registers, this function posts user information */
+  onRegister(register) {
+    this.setState({
+      register
+    });
+  }
+  /* When a user successfully logs in, this function updates the `user` property in state to that particular user */
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
 
-    if (movies.length === 0) return <div className="main-view">The list is empty</div>;
+  render() {
+    const {movies, selectedMovie, user, register} = this.state;
+
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView */
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    if (!register) return <RegistrationView onRegistration={register => this.onRegistration(register)} />;
+
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
       {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
         ))
       }
       </div>
@@ -41,3 +74,11 @@ export class MainView extends React.Component {
 }
 
 export default MainView;
+
+MainView.propTypes = {
+  movie: PropTypes.isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+  onBackClick: PropTypes.func.isRequired,
+  onLoggedIn: PropTypes.func.isRequired,
+  onRegistration: PropTypes.func.isRequired
+};
